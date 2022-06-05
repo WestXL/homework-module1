7e64fcba9092573800239730ed50cbd0d0345fea
WestXL <L.Xiao.191@westcliff.edu>
2022-06-05 11:18:30 -0700

    w5d3 hw2

--- js/activity.js
+++ js/activity.js
@@ -1,10 +1,23 @@
-$(document).ready(function(){
-    $("table tr").each(function(){
+$(document).ready(function () {
+    $("table tr").each(function () {
         $(this).find("td:not(:first)").attr('id', 'selectable');
         $(this).find('td:contains("Not")').attr('id', 'notSelectable');
     })
 
-    $("td[id='selectable']").click(function() {
+    $("td[id='selectable']").click(function () {
+        var content = $(this).text();
         $(this).toggleClass("highlight");
+
+        if ($(this).hasClass("highlight")) {
+            $("#displaySelected").css("visibility", "visible");
+            $("#displaySelected").css("margin-top", "2em");
+            $("#result").append("<p>" + content + "</p>");
+        } else {
+            $("#result p:contains(" + content + ")").remove();
+            if ($("#result").has("p").length == false) {
+                $("#displaySelected").css("visibility", "hidden");
+                $("#displaySelected").css("margin-top", "0");
+            }
+        }
     });
 });
