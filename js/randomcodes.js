4ae58aeab0dea6bbd61380e909ad95fed0420abe
WestXL <L.Xiao.191@westcliff.edu>
2022-06-15 19:53:48 -0700

    m2w3d1 hw1

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -26,13 +26,13 @@ document.getElementById("codes").innerHTML = generateCode();
 
 // Disable Button
 function disableButton(btnvalue) {
-    document.getElementById("submit").disabled = btnvalue;
+    document.getElementById("subBtn").disabled = btnvalue;
     if (btnvalue == true) {
-        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
-        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+        document.getElementById("subBtn").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("subBtn").style.color = "rgba(255, 255, 255, 0.5)";
     } else {
-        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
-        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+        document.getElementById("subBtn").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("subBtn").style.color = "rgba(255, 255, 255, 1)";
     }
 }
 
